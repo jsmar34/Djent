d712cddb896947596c0e327b52b2bfa29582158f
jsmar34 <jsmar34@gmail.com>
2018-08-09 11:39:52 +1000

    Add files via upload
    Added SignOut.php to destroy session and remove user cookies when sign out is pressed. Redirects to login page.

---
+++ SignOut.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="loginStyle.css">
+  </head>
+
+  <body>
+
+  <?php
+    session_unset();
+    session_destroy();
+    setcookie("user", "", time() - 3600);
+
+    $conn = mysqli_connect("localhost","root","", "jsmar34_djent") or die(mysql_error());
+
+    header("Location: login.php");
+
+
+
+
+   ?>
+  </body>
+
+</html>
